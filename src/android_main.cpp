1eb0284e828a906b62cb482410db4c73d35ea045
lambdadroid <lambdadroid@gmail.com>
2017-06-06 16:51:02 +0200

    Android: Cleanup android_main and remove daemon functionality
    Doesn't work and I don't really see any reason to have it. thermald
    should be always started using the init system which requires the
    applications to stay alive.

--- src/android_main.cpp
+++ src/android_main.cpp
@@ -32,132 +32,32 @@
 #include "thd_parse.h"
 #include <sys/file.h>
 
-// getdtablesize() is removed from bionic/libc in LPDK*/
-// use POSIX alternative available. Otherwise fail
-#  ifdef _POSIX_OPEN_MAX
-#   define   getdtablesize()	(_POSIX_OPEN_MAX)
-# endif
-// for AID_* constatns
+// for AID_* constants
 #include <private/android_filesystem_config.h>
-
-// getdtablesize() is removed from bionic/libc in LPDK*/
-// use POSIX alternative available. Otherwise fail
-#  ifdef _POSIX_OPEN_MAX
-#   define   getdtablesize()	(_POSIX_OPEN_MAX)
-# endif
 
 // poll mode
 int thd_poll_interval = 4; //in seconds
 
 bool thd_ignore_default_control = false;
 
-static int pid_file_handle;
 
-// Stop daemon
-static void daemonShutdown() {
-	if (pid_file_handle)
-		close(pid_file_handle);
+// SIGTERM & SIGINT handler
+void sig_int_handler(int signum) {
 	thd_engine->thd_engine_terminate();
 	sleep(1);
 	delete thd_engine;
-}
-
-// signal handler
-static void signal_handler(int sig) {
-	switch (sig) {
-	case SIGHUP:
-		thd_log_warn("Received SIGHUP signal. \n");
-		break;
-	case SIGINT:
-	case SIGTERM:
-		thd_log_info("Daemon exiting \n");
-		daemonShutdown();
-		exit(EXIT_SUCCESS);
-		break;
-	default:
-		thd_log_warn("Unhandled signal %s\n", strsignal(sig));
-		break;
-	}
-}
-
-static void daemonize(char *rundir, char *pidfile) {
-	int pid, sid, i;
-	char str[10];
-	struct sigaction sig_actions;
-	sigset_t sig_set;
-
-	if (getppid() == 1) {
-		return;
-	}
-	sigemptyset(&sig_set);
-	sigaddset(&sig_set, SIGCHLD);
-	sigaddset(&sig_set, SIGTSTP);
-	sigaddset(&sig_set, SIGTTOU);
-	sigaddset(&sig_set, SIGTTIN);
-	sigprocmask(SIG_BLOCK, &sig_set, NULL);
-
-	sig_actions.sa_handler = signal_handler;
-	sigemptyset(&sig_actions.sa_mask);
-	sig_actions.sa_flags = 0;
-
-	sigaction(SIGHUP, &sig_actions, NULL);
-	sigaction(SIGTERM, &sig_actions, NULL);
-	sigaction(SIGINT, &sig_actions, NULL);
-
-	pid = fork();
-	if (pid < 0) {
-		/* Could not fork */
-		exit(EXIT_FAILURE);
-	}
-	if (pid > 0) {
-		thd_log_info("Child process created: %d\n", pid);
-		exit(EXIT_SUCCESS);
-	}
-	umask(027);
-
-	sid = setsid();
-	if (sid < 0) {
-		exit(EXIT_FAILURE);
-	}
-	/* close all descriptors */
-	for (i = getdtablesize(); i >= 0; --i) {
-		close(i);
-	}
-
-	i = open("/dev/null", O_RDWR);
-	dup(i);
-	dup(i);
-	chdir(rundir);
-
-	pid_file_handle = open(pidfile, O_RDWR | O_CREAT, 0600);
-	if (pid_file_handle == -1) {
-		/* Couldn't open lock file */
-		thd_log_info("Could not open PID lock file %s, exiting\n", pidfile);
-		exit(EXIT_FAILURE);
-	}
-	/* Try to lock file */
-#ifdef LOCKF_SUPPORT
-	if (lockf(pid_file_handle, F_TLOCK, 0) == -1) {
-#else
-		if (flock(pid_file_handle,LOCK_EX|LOCK_NB) < 0) {
-#endif
-		/* Couldn't get lock on lock file */
-		thd_log_info("Couldn't get lock file %d\n", getpid());
-		exit(EXIT_FAILURE);
-	}
-	thd_log_info("Thermal PID %d\n", getpid());
-	snprintf(str, sizeof(str), "%d\n", getpid());
-	write(pid_file_handle, str, strlen(str));
+	exit(EXIT_SUCCESS);
 }
 
 static void print_usage(FILE* stream, int exit_code) {
 	fprintf(stream, "Usage:  thermald options [ ... ]\n");
 	fprintf(stream, "  --help Display this usage information.\n"
-			"  --version Show version.\n"
-			"  --no-daemon No daemon.\n"
+			"  --version Print thermald version and exit\n"
+			"  --test-mode Test Mode only: Allow non root user"
 			"  --poll-interval Poll interval 0 to disable.\n"
-			"  --exclusive_control To act as exclusive thermal controller.\n"
-			"  --config-file Configuration file to use other than the default config. \n");
+			"  --exclusive-control Take over thermal control from kernel thermal driver.\n"
+			"  --ignore-cpuid-check Ignore CPU ID check.\n"
+			"  --config-file Configuration file to use other than the default config.\n");
 
 	exit(exit_code);
 }
@@ -165,23 +65,20 @@ static void print_usage(FILE* stream, int exit_code) {
 int main(int argc, char *argv[]) {
 	int c;
 	int option_index = 0;
-	bool no_daemon = false;
-	bool exclusive_control = false;
 	bool test_mode = false;
-	bool is_privileged_user = false;
-	char *conf_file = NULL;
+	bool exclusive_control = false;
 	bool ignore_cpuid_check = false;
+	char *conf_file = NULL;
 
-	const char* const short_options = "hvnp:detc:";
+	const char* const short_options = "hvtp:eic:";
 	static struct option long_options[] = {
 			{ "help", no_argument, 0, 'h' },
 			{ "version", no_argument, 0, 'v' },
-			{ "no-daemon", no_argument, 0, 'n' },
-			{ "poll-interval", required_argument, 0, 'p' },
-			{ "exclusive_control", no_argument, 0, 'e' },
 			{ "test-mode", no_argument, 0, 't' },
-			{ "config-file", required_argument, 0, 'c' },
+			{ "poll-interval", required_argument, 0, 'p' },
+			{ "exclusive-control", no_argument, 0, 'e' },
 			{ "ignore-cpuid-check", no_argument, 0, 'i'},
+			{ "config-file", required_argument, 0, 'c' },
 			{ NULL, 0, NULL, 0 } };
 
 	if (argc > 1) {
@@ -192,11 +89,11 @@ int main(int argc, char *argv[]) {
 				print_usage(stdout, 0);
 				break;
 			case 'v':
-				fprintf(stdout, "1.1\n");
+				fprintf(stdout, "1.7.1\n");
 				exit(EXIT_SUCCESS);
 				break;
-			case 'n':
-				no_daemon = true;
+			case 't':
+				test_mode = true;
 				break;
 			case 'p':
 				thd_poll_interval = atoi(optarg);
@@ -204,14 +101,11 @@ int main(int argc, char *argv[]) {
 			case 'e':
 				exclusive_control = true;
 				break;
-			case 't':
-				test_mode = true;
+			case 'i':
+				ignore_cpuid_check = true;
 				break;
 			case 'c':
 				conf_file = optarg;
-				break;
-			case 'i':
-				ignore_cpuid_check = true;
 				break;
 			case -1:
 			case 0:
@@ -222,10 +116,9 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	is_privileged_user = (getuid() == 0) || (getuid() == AID_SYSTEM);
-	if (!is_privileged_user && !test_mode) {
-		thd_log_error("You do not have correct permissions to run thermal dameon!\n");
-		exit(1);
+	if (((getuid() != 0) && (getuid() != AID_SYSTEM)) && !test_mode) {
+		thd_log_error("You do not have correct permissions to run thermald!\n");
+		exit(EXIT_FAILURE);
 	}
 
 	if (mkdir(TDRUNDIR, 0755) != 0) {
@@ -235,15 +128,13 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 		}
 	}
-	mkdir(TDCONFDIR, 0755); // Don't care return value as directory
-	if (!no_daemon) {
-		daemonize((char *) "/tmp/", (char *) "/tmp/thermald.pid");
-	} else
-		signal(SIGINT, signal_handler);
+
+	signal(SIGINT, sig_int_handler);
+	signal(SIGTERM, sig_int_handler);
 
 	thd_log_info(
-			"Linux Thermal Daemon is starting mode %d : poll_interval %d :ex_control %d\n",
-			no_daemon, thd_poll_interval, exclusive_control);
+			"Linux Thermal Daemon is starting: poll_interval %d :ex_control %d\n",
+			thd_poll_interval, exclusive_control);
 
 	if (thd_engine_create_default_engine(ignore_cpuid_check, exclusive_control,
 			conf_file) != THD_SUCCESS) {
@@ -255,16 +146,11 @@ int main(int argc, char *argv[]) {
 	// when called with exit().
 	// Here just run for some time and gracefully return.
 	sleep(10);
-	if (pid_file_handle)
-		close(pid_file_handle);
 	thd_engine->thd_engine_terminate();
 	sleep(1);
 	delete thd_engine;
 #else
-	for (;;)
-		sleep(0xffff);
-
-	thd_log_info("Linux Thermal Daemon is exiting \n");
+	pthread_exit(NULL);
 #endif
 	return 0;
 }
